110f156e84cbf5b5a06db6a7eddca5143870f3b7
Arthur Manenti <arthur.manenti.videira@gmail.com>
2024-10-13 21:32:39 -0300

    Metodos em rotas

--- server.js
+++ server.js
@@ -1,20 +1,10 @@
-import http from "http"
+import app from './src/app.js'
 
 const PORT = 3000
 
-const rotas = {
-    "/": "curso de Express API",
-    "/livros": "Entrei na rota Livros",
-    "/autores": "Entrei na rota Autores"
-}
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" })
-    res.end(rotas[req.url])
-})
-
-server.listen(PORT, () => {
-    console.log("Escutando a porta 3000")
+app.listen(PORT, () => {
+    console.log(`Escutando a porta ${PORT}`)
 })
 
 
